b2d0379c2f5be7e001d1072bfbdd1a905317c3b9
Ido Harel <idohr1993@gmail.com>
2017-12-18 15:41:17 +0200

    nanny

---
+++ BE/Nanny.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    class Nanny
+    {
+        public long nannyId { get; set; }
+        public string lastNameNanny { get; set; }
+        public string firstNameNanny { get; set; }
+        public DateTime dateNanny { get; set; }
+        public long phoneNanny { get; set; }
+        public string addressNanny { get; set; }
+        public bool elevatorNanny { get; set; }
+        public int floorNanny { get; set; }
+        public int experienceNanny { get; set; }
+        public int maxChildNanny { get; set; }
+        public int minEgeChildNanny { get; set; }
+        public int maxEgeChildNanny { get; set; }
+        public bool isByHourNanny { get; set; }
+        public int rateHourNanny { get; set; }
+        public int rateMonthNanny { get; set; }
+        public bool[] daysWorkNanny { get; set; }
+        public DateTime[,] hoursWorkNanny { get; set; }
+        public bool isTamatNanny { get; set; }
+        public string recommendationsNanny { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
