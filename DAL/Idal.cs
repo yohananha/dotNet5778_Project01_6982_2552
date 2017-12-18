49fdad690fbac68984a0d8dc8406aa2452ad51e7
yohanan <yohananha@gmail.com>
2017-12-18 18:33:44 +0200

    numerable

--- DAL/Idal.cs
+++ DAL/Idal.cs
@@ -27,6 +27,8 @@ namespace DAL
         void updateContract(int idContractUpdate);
 
         IEnumerable<Nanny> getAllNanny(Func<Nanny, bool> Predicate=null);
-
+        IEnumerable<Mother> getAllMothers(Func<Mother, bool> Predicate = null);
+        IEnumerable<Child> getKidsByMoms(Func<Child, bool> Predicate = null);
+        IEnumerable<Contract> getContracts(Func<Contract, bool> Predicate = null);
     }
 }
