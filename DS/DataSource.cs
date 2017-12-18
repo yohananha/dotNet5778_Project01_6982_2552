da3ab61f216816febfc10d7b699f5133ec28cd60
Ido Harel <idohr1993@gmail.com>
2017-12-18 18:17:54 +0200

    dal

---
+++ DS/DataSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace DS
+{
+    class DataSource
+    {
+        public static List<Nanny> nannyList = new List<Nanny>();
+        public static List<Mother> motherList = new List<Mother>();
+        public static List<Child> childList = new List<Child>();
+        public static List<Contract> contractList = new List<Contract>();
+
+        //public static DataSource ds = null;
+
+        //public static DataSource GetDs()
+        //{
+        //    if (ds == null)
+        //        ds = new DataSource();
+        //    return ds;
+        //}
+
+        //private DataSource()
+        //{
+        //    nannyList = new List<Nanny>();
+        //    motherList = new List<Mother>();
+        //    childList = new List<Child>();
+        //    contractList = new List<Contract>();
+        //}
+    }
+}
