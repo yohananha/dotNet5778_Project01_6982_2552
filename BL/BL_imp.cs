a47f2b2b9402d865dc01ef9b954865b47459aa9c
Ido Harel <idohr1993@gmail.com>
2017-12-19 12:34:02 +0200

    BL interfac Ibl factoryDAL

--- BL/BL_imp.cs
+++ BL/BL_imp.cs
@@ -3,10 +3,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
 
 namespace BL
 {
-    class BL_imp
+    class BL_imp : Ibl
     {
+        DAL.Idal dal;
+
+        public BL_imp()
+        {
+            dal = DAL.factoryDal.getDal();
+        }
+
+        public void addChild(Child child)
+        {
+            dal.addChild(child);
+        }
+
+        public void addContract(Contract contract)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addMom(Mother mother)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addNanny(Nanny nanny)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteChild(long idChildDel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteContract(int idContractDel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteMother(long idMotherDel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteNanny(long idNannyDel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Mother> getAllMothers(Func<Mother, bool> Predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Nanny> getAllNanny(Func<Nanny, bool> Predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Contract> getContracts(Func<Contract, bool> Predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Child> getKidsByMoms(Func<Child, bool> Predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateChild(Child child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateContract(Contract contract)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateMother(Mother mother)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateNanny(Nanny nannye)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ DAL/factoryDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class factoryDal
+    {
+        public static Idal getDal()
+        {
+            return new DAL_imp();
+        }
+    }
+}
