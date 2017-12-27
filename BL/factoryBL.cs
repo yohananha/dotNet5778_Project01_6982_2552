681bf167783a770671bd4427a0a82a1e3c6e03ec
Ido Harel <idohr1993@gmail.com>
2017-12-27 23:21:51 +0200

    console

---
+++ BL/factoryBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class factoryBL
+    {
+        public static Ibl getBL()
+        {
+            return new BL_imp();
+        }
+    }
+}
