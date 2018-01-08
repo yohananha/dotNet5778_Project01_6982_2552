ab5f69c3d2b4e6ead9af51473e65f05fc826e87d
yohanan <yohananha@gmail.com>
2018-01-08 23:51:50 +0200

    PLWPF

---
+++ PLWPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BL;
+using BE;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void momSbutton_Click(object sender, RoutedEventArgs e)
+        {
+            var mom = new MomWindow();
+            mom.ShowDialog();
+        }
+
+        private void kids_Click(object sender, RoutedEventArgs e)
+        {
+            var kid = new KidaWindow();
+            kid.ShowDialog();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var nanny = new NannyWindow();
+            nanny.ShowDialog();
+        }
+
+        private void contract_Click(object sender, RoutedEventArgs e)
+        {
+            var contract = new ContractWindow();
+            contract.ShowDialog();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var data = new DataWindow();
+            data.ShowDialog();
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
